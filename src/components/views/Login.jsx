7c54b0cc1f009636905c78fde0f43d7b9cddbd59
maxivega0 <maximilianovega600@gmail.com>
2023-06-13 17:35:32 -0300

    agregamos funcoin iniciar sesion encargada de verificar si el usuario ingresado existe o no

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,5 +1,6 @@
 import { Form, Button, Container, Card } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import { iniciarSesion } from "../../helpers/queries";
 
 const Login = () => {
   // Declaramos el hook use form
@@ -15,6 +16,17 @@ const Login = () => {
 
   const onSubmit = (usuario) => {
     console.log(usuario);
+      // then implica lo siguiente: yo ejecutare una funcion, una vez que se ejecute iniciar sesion, espera que se ejecute y entonces, realiza lo siguiente
+      // respuesta es una variable inventada que va a contener el return de "inciarSesion"
+      iniciarSesion(usuario).then((respuesta)=>{
+        if (respuesta) {
+          console.log("aqui esta todo bien con el usuario");
+        }else{
+          console.log("Email o contraseña incorrectos");
+        }
+      })
+
+    reset();
   };
 
   return (
@@ -26,27 +38,43 @@ const Login = () => {
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
               <Form.Control
-                type="text"
+                type="email"
                 placeholder="Ingrese un email"
                 // operador spread, asi invocamos al objeto register
                 {...register("email", {
                   required: "El email es un dato obligatorio",
                   // expresion regular
                   pattern: {
-                    value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=? ^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a -z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                    message:"El email debe tener el siguiente formato: 'mail@dominio.com'"
-                  }
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=? ^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a -z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El email debe tener el siguiente formato: 'mail@dominio.com'",
+                  },
                 })}
               />
               <Form.Text className="text-danger">
                 {/*  */}
-                {errors.email?.message }
+                {errors.email?.message}
               </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", {
+                  required: "El password es obligatorio",
+                  pattern: {
+                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message: `La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.
+                 NO puede tener otros símbolos.`,
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
             </Form.Group>
             <Button variant="primary" type="submit">
               Ingresar
