c67c6d001f8f7dfb11f068e67ba7e6c19ac5da92
maxivega0 <maximilianovega600@gmail.com>
2023-07-13 18:04:06 -0300

    format al doc

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,9 +1,8 @@
 import { Button } from 'react-bootstrap';
-import error from '../../assets/error404.png'
 const Error404 = () => {
     return (
         <section className="mainSection text-center">
-            <img src={error} alt="error 404" />
+            <img src={"https://images.pexels.com/photos/1888015/pexels-photo-1888015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} alt="error 404" />
             <div>
             <Button variant='primary' >Volver al inicio</Button>
 
