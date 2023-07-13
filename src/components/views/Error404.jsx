1a42d608eb27da317a251dd2a31b31f005df16f7
maxivega0 <maximilianovega600@gmail.com>
2023-07-13 18:04:29 -0300

    format al doc

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,14 +1,18 @@
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 const Error404 = () => {
-    return (
-        <section className="mainSection text-center">
-            <img src={"https://images.pexels.com/photos/1888015/pexels-photo-1888015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} alt="error 404" />
-            <div>
-            <Button variant='primary' >Volver al inicio</Button>
-
-            </div>
-        </section>
-    );
+  return (
+    <section className="mainSection text-center">
+      <img
+        src={
+          "https://images.pexels.com/photos/1888015/pexels-photo-1888015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+        alt="error 404"
+      />
+      <div>
+        <Button variant="primary">Volver al inicio</Button>
+      </div>
+    </section>
+  );
 };
 
 export default Error404;
